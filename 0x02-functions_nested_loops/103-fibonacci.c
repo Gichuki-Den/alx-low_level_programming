b0b86f964087be97bb2c5a16cff4d0c47e6a8f6a
Joshua Ikem <niphemijoshua@gmail.com>
2023-06-20 16:25:21 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main ()
+{
+	int first = 1;
+	int second = 2;
+	int next = 0;
+	int sum = 0;
+
+	while (first <= 4000000)
+	{
+		if (first % 2 == 0)
+		{
+			sum += first;
+		}
+
+		next = first + second;
+		first = second;
+		second = next;
+	}
+
+	printf("%d\n", sum);
+	return 0;
+}
