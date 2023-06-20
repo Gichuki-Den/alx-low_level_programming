93f2db402d082bd2d94674d0bbf6dd8544be4a5b
Joshua Ikem <niphemijoshua@gmail.com>
2023-06-20 16:17:49 -0700

    joe

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,21 +8,24 @@
 void fib(void)
 {
 	int i;
-	unsigned int f = 1;
-	unsigned int s = 2;
-	unsigned int r;
+	unsigned long f = 1;
+	unsigned long s = 2;
+	unsigned long r;
 
-	for (i = 0; i < 50; i++)
+	for (i = 0; i <= 50; i++)
 	{
 		if (i == 0)
 		{
 			printf("%u, %u, ", f, s);
 			continue;
 		}
-		r = f + s;
-		printf("%u, ", r);
-		f = s;
-		s = r;
+		else if (i > 0)
+		{
+			r = f + s;
+			printf("%u, ", r);
+			f = s;
+			s = r;
+		}
     }
 }
 
