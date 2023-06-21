fab3aa5d8384de30b1926910070fe3bbd1a9b337
Joshua Ikem <niphemijoshua@gmail.com>
2023-06-20 19:07:24 -0700

    try

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
-/** fibo_seq - to print fibo numbers
-* main - return o and call the fibo_seq function
+/**
+* fibo_seq - prints fibonacci numbers of first 98 order
 */
 void fibo_seq(void)
 {
-	unsigned int x= 2;
-	unsigned int y = 1;
-	unsigned int z;
-	unsigned int count = 2;
+	unsigned long long x = 2;
+	unsigned long long y = 1;
+	unsigned long long z;
+	unsigned long long count = 2;
 
 	printf("1, 2");
 
 	while (count < 98)
 	{
 		z = x + y;
-		printf(", %u, ", z);
+		printf(", %llu, ", z);
 		x = z;
 		y = x;
-		
 		count++;
 	}
 	printf("\n");
 }
+/**
+ * main - return o and call the fibo_seq function
+ * return 0 always 
+ */
 int main(void)
 {
 	fibo_seq();
