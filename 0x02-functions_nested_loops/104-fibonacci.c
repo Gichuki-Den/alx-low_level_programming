09cf53984e2c0d318b91895b8565ff6cafef16a2
Joshua Ikem <niphemijoshua@gmail.com>
2023-06-21 09:58:23 -0700

    final

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,45 +1,36 @@
 #include <stdio.h>
 /**
-* main -A program that print the first 98 fibonacci numbers.
-* Return: Nothing.
+* fibo_seq - prints the first 98 fibonacci numbers
+*/
+void fibo_seq(void)
+{	unsigned long x = 0;
+	unsigned long y = 1;
+	unsigned long z;
+	unsigned long i = 0;
+	
+	while (i <= 98)
+	{
+		z = x + y;
+		if (i <= 97)
+		{
+			printf("%lu, ", z);
+			x = y;
+			y = z;
+		}
+		else
+		{
+			printf("%lu", z);
+		}
+		count++;
+	}
+	printf("\n");
+}
+/**
+* main - return 0 and call fibo_seq function
+* Return: Always  0
 */
 int main(void)
 {
-	int fibonacci;
-	unsigned long num1, num2, num3;
-	unsigned long m, n, p, carry;
-
-	fibonacci = 0;
-	num1 = 0;
-	num2 = 1;
-	for (fibonacci = 1; fibonacci <= 91; fibonacci++)
-	{
-		num3 = num1 + num2;
-		num1 = num2;
-		num2 = num3;
-		printf("%lu, ", num3);
-	}
-	m = num1 % 1000;
-	num1 = num1 / 1000;
-	n = num2 % 1000;
-	num2 = num2 / 1000;
-	while (fibonacci <= 98)
-	{
-		carry = (m + n) / 1000;
-		p = (m + n) - carry * 1000;
-		num3 = (num1 + num2) + carry;
-		m = n;
-		n = p;
-		num1 = num2;
-		num2 = num3;
-		if (p >= 10)
-			printf("%lu%lu", num3, p);
-		else
-			printf("%lu0%lu", num3, p);
-		if (fibonacci != 98)
-			printf(", ");
-		fibonacci++;
-	}
-	putchar('\n');
+	fibo_seq();
 	return (0);
 }
