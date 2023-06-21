be806d4f03386c44e39dc018b6cc627753598edf
Joshua Ikem <niphemijoshua@gmail.com>
2023-06-20 20:12:22 -0700

    peace be still

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,9 +7,9 @@ void fibo_seq(void)
 	unsigned long x = 0;
 	unsigned long y = 1;
 	unsigned long z;
-	unsigned long count = 1;
+	unsigned long count = 3;
 
-	while (count <= 98)
+	while (count < 98)
 	{
 		z = x + y;
 		printf("%lu, ", z);
