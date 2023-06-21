f7d4880ed02ea378bb056fd791097d63f498d7a4
Joshua Ikem <niphemijoshua@gmail.com>
2023-06-20 18:46:46 -0700

    hisense

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+/** fibo_seq - to print fibo numbers
+* main - return o and call the fibo_seq function
+*/
 void fibo_seq(void)
 {
-	int current = 1;
-	int next = 2;
+	int current = 2;
+	int next = 1;
 	int count = 2;
 
 	printf("1, 2");
 
 	while (count < 98)
 	{
-		next = next + current;
-		current = next - current;
-		count++;
-
+		x + y = z;
+		print(", %u, ", z);
+		x = z;
+		y = x;
+		
 		count++;
 	}
 	printf("\n");
